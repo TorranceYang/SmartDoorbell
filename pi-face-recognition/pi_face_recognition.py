5ca1a567be9d7a2bb35da92f7df910ffd730d30d
David Woods <dwoo@microsoft.com>
2018-07-24 16:17:11 -0700

    Pulling face identification into a separate file and class
    - Loads model on __init__, but can load again later after updating
    - Takes a BGR frame and a set of rects, one for each detected face
    - Returns a list of the names of all identified people, if any

--- pi-face-recognition/pi_face_recognition.py
+++ pi-face-recognition/pi_face_recognition.py
@@ -10,6 +10,7 @@ import imutils
 import pickle
 import time
 import cv2
+import face_identifier
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -34,6 +35,8 @@ time.sleep(2.0)
 # start the FPS counter
 fps = FPS().start()
 
+identifier = face_identifier.FaceIdentifier(args["encodings"])
+
 # loop over frames from the video file stream
 while True:
 	# grab the frame from the threaded video stream and resize it
@@ -41,10 +44,8 @@ while True:
 	frame = vs.read()
 	frame = imutils.resize(frame, width=500)
 	
-	# convert the input frame from (1) BGR to grayscale (for face
-	# detection) and (2) from BGR to RGB (for face recognition)
+	# convert the input frame from BGR to grayscale (for face detection)
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
 	# detect faces in the grayscale frame
 	rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
@@ -56,39 +57,7 @@ while True:
 	# need to do a bit of reordering
 	boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rects]
 
-	# compute the facial embeddings for each face bounding box
-	encodings = face_recognition.face_encodings(rgb, boxes)
-	names = []
-
-	# loop over the facial embeddings
-	for encoding in encodings:
-		# attempt to match each face in the input image to our known
-		# encodings
-		matches = face_recognition.compare_faces(data["encodings"],
-			encoding)
-		name = "Unknown"
-
-		# check to see if we have found a match
-		if True in matches:
-			# find the indexes of all matched faces then initialize a
-			# dictionary to count the total number of times each face
-			# was matched
-			matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-			counts = {}
-
-			# loop over the matched indexes and maintain a count for
-			# each recognized face face
-			for i in matchedIdxs:
-				name = data["names"][i]
-				counts[name] = counts.get(name, 0) + 1
-
-			# determine the recognized face with the largest number
-			# of votes (note: in the event of an unlikely tie Python
-			# will select first entry in the dictionary)
-			name = max(counts, key=counts.get)
-		
-		# update the list of names
-		names.append(name)
+	names = identifier.IdentifyFaces(frame, rects)
 
 	# loop over the recognized faces
 	for ((top, right, bottom, left), name) in zip(boxes, names):
